dac88064fa0e2af4eebcb2be3f0f5b3d982f98fb
erik.palencik <erik.palencik@exe.sk>
2024-07-27 21:04:08 +0200

    My try to make cmake

---
+++ subfolder/test_sub.cpp
@@ -0,0 +1,12 @@
+#include "test_sub.h"
+
+#include <iostream>
+
+namespace subfolder {
+	void Subfolder()
+	{
+		std::cout << "Here" << std::endl;
+	}
+}
+
+
---
+++ subfolder/test_sub.h
@@ -0,0 +1,5 @@
+
+
+namespace subfolder {
+	void Subfolder();
+}
