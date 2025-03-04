ed34b11f5fadd5587d8055e9a575b2f420c58772
exevrs <vrs@exe.sk>
2025-03-04 21:27:02 +0100

    Initial commit

--- BevySim/src/main.rs
+++ BevySim/src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .add_systems(Startup, startup_scripts)
+        .run();
+}
+
 #[derive(Component)]
 struct Position {
     x: f32,
@@ -12,12 +20,13 @@ fn print_position_system(query: Query<&Position>) {
     }
 }
 
+fn startup_scripts(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite::from_image(
+        asset_server.load("res/textures/vzv.png"),
+    ));
+}
 
 fn hello_world() {
     println!("hello world!");
-}
-
-
-fn main() {
-    App::new().add_systems(Update, hello_world).run();
 }
