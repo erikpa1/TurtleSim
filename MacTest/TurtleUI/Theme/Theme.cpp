8b8acf786da44ee9bd8a9da307206be925a4288f
Erik Palencik <erik.palencik@outlook.com>
2025-09-17 22:04:06 +0200

    Update turtle AI theme

---
+++ MacTest/TurtleUI/Theme/Theme.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erik Palencik on 17/09/2025.
+//
+
+#include "Theme.h"
---
+++ MacTest/TurtleUI/Theme/Theme.h
@@ -0,0 +1,20 @@
+//
+// Created by Erik Palencik on 17/09/2025.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+
+class Theme {
+  public:
+
+  sf::Color ButtonColor = sf::Color(100, 100, 100);
+  sf::Color ButtonColoHover = sf::Color(150, 150, 150);
+
+
+};
+
+
+
